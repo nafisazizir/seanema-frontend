29c1c679a8519e20573a7e8b8332ae6f0c17f667
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-06-30 18:33:22 +0700

    feat: add hover navbar & hero title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import "./HomeStyle.css";
 
 const Home = () => {
   document.body.style.backgroundColor = "var(--color-white-100)";
   return (
     <>
       <Navbar />
-      Home
+      <div className="movies-page">
+        <h2 className="hero-title">Now Playing in Seanema ⬇️</h2>
+      </div>
     </>
   );
 };
