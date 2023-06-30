b8f3438cbcfaa3c3047c84e08d565f2323588cb3
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-06-30 19:09:06 +0700

    feat: add movie card

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./MovieCardStyle.css";
+
+interface MovieCardProps {
+  id: string;
+  title: string;
+  age_rating: string;
+  poster_url: string;
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({
+  id,
+  title,
+  age_rating,
+  poster_url,
+}) => {
+  return (
+    <div className="movie-card-container">
+      <div
+        className="poster"
+        style={{
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundImage: `url(${poster_url})`,
+        }}
+      ></div>
+      <div>
+        <h6 className="card-movie-title">{title}</h6>
+        <div className="headline-text age-rating">{age_rating}+</div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import "./HomeStyle.css";
+import MovieCard from "../components/MovieCard/MovieCard";
 
 const Home = () => {
   document.body.style.backgroundColor = "var(--color-white-100)";
@@ -9,6 +10,12 @@ const Home = () => {
       <Navbar />
       <div className="movies-page">
         <h2 className="hero-title">Now Playing in Seanema ⬇️</h2>
+        <MovieCard
+          id="1"
+          title="Fast X"
+          poster_url="https://image.tmdb.org/t/p/w500/fiVW06jE7z9YnO4trhaMEdclSiC.jpg"
+          age_rating="15"
+        />
       </div>
     </>
   );
