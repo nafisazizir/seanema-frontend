e8073dc65fe82dd70bd1ccb77139c7ef35bd27ac
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-07-02 19:28:03 +0700

    feat: add seats base page

--- src/pages/Book/Book.tsx
+++ src/pages/Book/Book.tsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate, useLocation } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import UserDataService from "../../services/user";
 import AgeRatingModal from "./AgeRatingModal";
+import "./BookStyle.css";
 
 const Book = () => {
   const { showtimeId } = useParams();
@@ -35,11 +36,55 @@ const Book = () => {
     fetchMovie();
   }, []);
 
+  const [selectedSeats, setSelectedSeats] = useState<number[]>([]);
+
+  const handleSeatClick = (seat: number) => {
+    if (selectedSeats.includes(seat)) {
+      setSelectedSeats(
+        selectedSeats.filter((selectedSeat) => selectedSeat !== seat)
+      );
+    } else {
+      setSelectedSeats([...selectedSeats, seat]);
+    }
+  };
+
+  const renderSeats = () => {
+    const seats = [];
+
+    for (let seat = 1; seat <= 64; seat++) {
+      const seatDiv = (
+        <div className="seat seat-available">
+          <div className="paragraph-normal">{seat}</div>
+        </div>
+      );
+      seats.push(seatDiv);
+    }
+
+    return seats;
+  };
+
   return (
     <>
       <Navbar />
       <AgeRatingModal isOpen={!isAllowed} onClose={handleNotAllowedUser} />
-      <div className="page-template">Book</div>
+      <div className="page-template">
+        <div className="seat-legend paragraph-small">
+          <div className="seat-legend-item">
+            <div className="seat seat-disabled" />
+            <div>Not Available</div>
+          </div>
+          <div className="seat-legend-item">
+            <div className="seat seat-available" />
+            <div>Available</div>
+          </div>
+          <div className="seat-legend-item">
+            <div className="seat seat-selected" />
+            <div>Your Choice</div>
+          </div>
+        </div>
+
+        <div className="seat-grid">{renderSeats()}</div>
+      </div>
     </>
   );
 };
